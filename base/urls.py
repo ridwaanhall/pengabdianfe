45349280c7e6e177222ca27014ef3c2c9d776876
ridwaanhall <erbyl14@gmail.com>
2024-08-04 09:32:01 +0700

    Pamong

--- base/urls.py
+++ base/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list-pamong/', views.list_pamong, name='list-pamong'),
+    path('tambah-pamong/', views.tambah_pamong, name='tambah-pamong'),
 ]
--- base/views.py
+++ base/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
+
+def list_pamong(request):
+    return render(request, 'list-pamong.html')
+
+def tambah_pamong(request):
+    return render(request, 'tambah-pamong.html')
