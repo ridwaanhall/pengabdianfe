766d66a9946a817dfcc9e18b6ef030d52acf3555
ridwaanhall <erbyl14@gmail.com>
2024-08-05 21:22:18 +0700

    forms user. not used

--- base/forms.py
+++ base/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from .models import Pamong
+from .models import Pamong, User
 
 class PamongForm(forms.ModelForm):
     class Meta:
@@ -9,3 +9,10 @@ class PamongForm(forms.ModelForm):
             'pekerjaan', 'gol_darah', 'jenis_kelamin', 'agama', 'nip', 'jabatan',
             'masa_jabatan_mulai', 'masa_jabatan_selesai', 'pendidikan_terakhir', 'foto'
         ]
+
+class UserForm(forms.ModelForm):
+    password_text = forms.CharField(widget=forms.PasswordInput)
+
+    class Meta:
+        model = User
+        fields = ['pamong', 'username', 'password_text', 'email', 'is_admin']
