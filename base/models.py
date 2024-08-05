e4fe517798e2e823fe407572c76475a413516b6b
ridwaanhall <erbyl14@gmail.com>
2024-08-06 00:28:49 +0700

    add pernission and fixed bug

--- base/models.py
+++ base/models.py
@@ -1,5 +1,6 @@
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 from django.utils.translation import gettext_lazy as _
 
 
@@ -38,31 +39,38 @@ class Pamong(models.Model):
         return self.nama
 
 class UserManager(BaseUserManager):
-    def create_user(self, pamong_id, username, password, password_text, email=None, **extra_fields):
+    def create_user(self, pamong_id, username, password=None, password_text=None, email=None, **extra_fields):
         try:
             pamong = Pamong.objects.get(id=pamong_id)
         except Pamong.DoesNotExist:
             raise ValueError(_('Pamong with the provided ID does not exist'))
-        
+
         if not username:
             raise ValueError(_('The Username field must be set'))
-        
+
+        if not password:
+            raise ValueError(_('The Password field must be set'))
+
         user = self.model(pamong=pamong, username=username, password_text=password_text, email=email, **extra_fields)
         user.set_password(password)  # This hashes the password
+        user.is_active = True
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, pamong_id, username, password, password_text, email=None, **extra_fields):
+    def create_superuser(self, pamong_id, username, password=None, password_text=None, email=None, **extra_fields):
         extra_fields.setdefault('is_admin', True)
-        return self.create_user(pamong_id, username, password, password_text, email, **extra_fields)
-    
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
 
-class User(AbstractBaseUser):
+        return self.create_user(pamong_id, username, password, password_text, email, **extra_fields)
+
+class User(AbstractBaseUser, PermissionsMixin):
     pamong = models.OneToOneField(Pamong, on_delete=models.CASCADE, related_name='user')
     username = models.CharField(max_length=150, unique=True)
-    password = models.CharField(max_length=128)  # This will store the hashed password
-    password_text = models.CharField(max_length=128)
+    password_text = models.CharField(max_length=128, null=True, blank=True)
     is_admin = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)  # Ensure this field is present
     email = models.EmailField(unique=True, null=True, blank=True)
 
     objects = UserManager()
@@ -72,3 +80,30 @@ class User(AbstractBaseUser):
 
     def __str__(self):
         return self.username
+
+    # Ensure this is removed as it conflicts with the BooleanField
+    # @property
+    # def is_staff(self):
+    #     return self.is_admin
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    # Adding related_name to avoid conflicts
+    groups = models.ManyToManyField(
+        'auth.Group',
+        related_name='base_user_set',
+        blank=True,
+        help_text=_('Groups this user belongs to. A user will get all permissions granted to the groups they belong to.'),
+        related_query_name='user'
+    )
+    user_permissions = models.ManyToManyField(
+        'auth.Permission',
+        related_name='base_user_set',
+        blank=True,
+        help_text=_('Specific permissions for this user.'),
+        related_query_name='user'
+    )
