25143cd3bdcf64acd5750c4b39d625a0c4bd500d
ridwaanhall <erbyl14@gmail.com>
2024-08-06 00:29:03 +0700

    add login api

--- api/views.py
+++ api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from .serializers import LoginSerializer
 
-# Create your views here.
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+    permission_classes = [AllowAny]
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        token = serializer.create(serializer.validated_data)
+        return Response(token, status=status.HTTP_200_OK)
