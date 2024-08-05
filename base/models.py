b118e268b0f51647e3dd27905463d48e0d1c18c0
ridwaanhall <erbyl14@gmail.com>
2024-08-05 17:48:20 +0700

    add Pamong table

--- base/models.py
+++ base/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
-# Create your models here.
+class Pamong(models.Model):
+    nama = models.CharField(max_length=100)
+    nik = models.CharField(max_length=16, unique=True)
+    nip = models.CharField(max_length=18, unique=True, blank=True, null=True)
+    tempat_lahir = models.CharField(max_length=50, blank=True, null=True)
+    tanggal_lahir = models.DateField(blank=True, null=True)
+    alamat = models.TextField(blank=True, null=True)
+    status_kawin = models.CharField(max_length=20, choices=[
+        ("Belum Kawin", "Belum Kawin"),
+        ("Kawin", "Kawin"),
+        ("Cerai Hidup", "Cerai Hidup"),
+        ("Cerai Mati", "Cerai Mati")
+    ], blank=True, null=True)
+    pekerjaan = models.CharField(max_length=50, blank=True, null=True)
+    jabatan = models.CharField(max_length=50, blank=True, null=True)
+    gol_darah = models.CharField(max_length=2, choices=[
+        ("A", "A"),
+        ("B", "B"),
+        ("AB", "AB"),
+        ("O", "O")
+    ], blank=True, null=True)
+    agama = models.CharField(max_length=20, blank=True, null=True)
+    jenis_kelamin = models.CharField(max_length=1, choices=[
+        ("L", "L"),
+        ("P", "P")
+    ], blank=True, null=True)
+    masa_jabatan_mulai = models.IntegerField(blank=True, null=True)
+    masa_jabatan_selesai = models.IntegerField(blank=True, null=True)
+    pendidikan_terakhir = models.CharField(max_length=50, blank=True, null=True)
+    foto = models.ImageField(upload_to='images/', blank=True, null=True)
+
+    def __str__(self):
+        return self.nama
